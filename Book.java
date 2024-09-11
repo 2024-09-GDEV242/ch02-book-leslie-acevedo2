71f6d9457563b9cc02b1c84509760624f8b29f57
leslie-acevedo2 <G00300763@stu.raritanval.edu>
2024-09-11 18:46:16 -0400

    Update Book.java
    boolean course text

--- Book.java
+++ Book.java
@@ -12,16 +12,20 @@ public class Book
     private String author;
     private String title;
     private int pages;
-
+    private String refNumber = "refNumber";
+    private int borrowed;
+    private boolean courseText; 
+    
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages)
+    public Book(String bookAuthor, String bookTitle, int bookPages, boolean isCourseText)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        courseText = isCourseText
     }
 
     // Add the methods here ...
@@ -34,9 +38,17 @@ public class Book
     public int getPages (){
         return pages;
     }
-    public void printDetails(){
-        System.out.printIn("Robin Crusoe"+ Robin Crusoe + ",Daniel Defoe" + Daniel Defoe + ",232" + 232);
+    public void printAuthor (){
+        System.out.println("Author:" + author);
     }
+    public void printTitle() {
+        System.out.println("Title:" + title);
+    }
+    public void printDetails(){
+        System.out.println("Title:"+ title + ",Author" + author + "Pages" + pages);
+    }
+    if (refNumber.length() > 0)
 
-    
+    {System.outprintln("ReferenceNumber:" + refNumber);
 }
+else { 
