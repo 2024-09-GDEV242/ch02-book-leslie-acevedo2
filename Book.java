aa50f52d2d11c3e039bb9558df016438286fa997
leslie-acevedo2 <G00300763@stu.raritanval.edu>
2024-09-10 20:50:34 -0400

    Update Book.java
    added print details method

--- Book.java
+++ Book.java
@@ -17,10 +17,11 @@ public class Book
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     // Add the methods here ...
@@ -29,6 +30,12 @@ public class Book
     }
     public String getTitle (){
         return title;
+    }
+    public int getPages (){
+        return pages;
+    }
+    public void printDetails(){
+        System.out.printIn("Robin Crusoe"+ Robin Crusoe + ",Daniel Defoe" + Daniel Defoe + ",232" + 232);
     }
 
     
